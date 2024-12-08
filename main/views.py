56437167c60c5ac698d9980f11cc9a310c733bd6
Timur Safiev <tsafiev285@gnail.com>
2024-12-08 09:05:13 +0300

    create base project, created a templates folder in the main folder, added two functions
    created a templates folder in the main folder, added two functions in the Sushi_Osaka

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Суши Осака',
+        'content': 'Добро пожаловать на сайт Суши Осака'
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Суши Осака - О нас',
+        'content':'О нас',
+        'text_on_page': 'Текст какие мы класные'
+    }
+
+    return render(request, 'main/about.html', context)
