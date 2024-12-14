0a1f50f93590d23246160b0f8fddd14a968d3daf
Timur Safiev <tsafiev285@gnail.com>
2024-12-14 11:04:10 +0300

    Simple search created

---
+++ goods/utils.py
@@ -0,0 +1,16 @@
+from django.db.models import Q
+
+from goods.models import Products
+
+def q_search(query):
+    if query:
+        return Products.objects.filter(name__icontains=query)
+    
+    keywords = [word for word in query.split(' ') if len(word) > 2]
+    
+    q_objects = Q()
+    
+    for token in keywords:
+        q_objects |= Q(name__icontains=token)
+        
+    return Products.objects.filter(q_objects)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./main/templates/main/base.html",
+        "./main/templates/main/index.html",
+        "./main/templates/main/about.html",
+        "./goods/templates/goods/catalog.html",
+        "./goods/templates/goods/product.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
