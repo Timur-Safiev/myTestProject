6e61ffeeef3f9f4258ec19b1f01934f5578c9ead
Timur Safiev <tsafiev285@gnail.com>
2024-12-17 19:44:26 +0300

    Login required decorator-redirect to target page

--- users/views.py
+++ users/views.py
@@ -16,6 +16,9 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы успешно вошли в аккаунт")
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -63,7 +66,7 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
-
+@login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы успешно вышли из аккаунта")
     auth.logout(request)
